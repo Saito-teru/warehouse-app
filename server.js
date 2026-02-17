8bc1842337b3a63d9b2ef6e8d44300295a360dfb
Saito <saito-t.i1b@nhk-g.co.jp>
2026-02-17 12:38:34 +0900

    add login page and enable static public folder

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
+
 
 // /api 配下に auth ルートを接続（/api/login）
 app.use("/api", authRoutes);
